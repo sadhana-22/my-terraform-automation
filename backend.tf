9ded4c6c3a7e5d580cd4ae5da6b2194896f090b6
sadhana-22 <160120749+sadhana-22@users.noreply.github.com>
2025-01-02 18:49:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket = "young-minds-app-22"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "terraform-dynamodb
-"
+    dynamodb_table = "terraform-dynamodb"
   }
 }
