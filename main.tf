939071a9869a43abed095ae919d18ea6d9daa9da
sadhana-22 <160120749+sadhana-22@users.noreply.github.com>
2025-01-28 13:38:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 }
 
 #Create security group with firewall rules
-resource "aws_security_group" "jenkins-2203-sg-new" {
+resource "aws_security_group" "jenkins-2203-sg" {
   name        = var.security_group
   description = "security group for Ec2 instance"
 
