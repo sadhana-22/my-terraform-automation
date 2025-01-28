2829cf11d3c6d8b8c1a0f9901c713dd94e9e2f47
sadhana-22 <160120749+sadhana-22@users.noreply.github.com>
2025-01-28 11:02:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2203.id]
+  vpc_security_group_ids = [aws_security_group.jenkins-2203-sg.id]
   tags= {
     Name = var.tag_name
   }
